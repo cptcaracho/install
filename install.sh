00d261569307050c8a9f53d7e5fa969ba05406ca
twinaxxe <55756409+twinaxxe@users.noreply.github.com>
2023-06-02 14:36:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm xorg sddm dhcpcd kate konsole yakuake dolphin breeze krunner plasma-desktop kscreen packagekit-qt5 plasma-firewall firewalld iproute2 ufw plasma-integration ark kmix skanlite breeze-gtk plasma-systemmonitor systemsettings khotkeys neovim keepass dolphin-plugins kdesu chromium obsidian evolution fish vlc rofi dmenu i3-wm telegram-desktop gimp onboard partitionmanager
+sudo pacman -Syu --noconfirm xorg sddm dhcpcd kate konsole yakuake dolphin breeze krunner plasma-desktop kscreen packagekit-qt5 plasma-firewall firewalld iproute2 ufw plasma-integration ark kmix skanlite breeze-gtk plasma-systemmonitor systemsettings khotkeys neovim keepass dolphin-plugins kdesu chromium obsidian evolution fish vlc rofi dmenu i3-wm telegram-desktop gimp onboard partitionmanager syncthing
+sudo systemctl start sddm
+sudo systemctl enable sddm
+sudo systemctl start dhcpcd
+sudo systemctl enable dhcpcd
+sudo systemctl start syncthing
+sudo systemctl enable syncthing
